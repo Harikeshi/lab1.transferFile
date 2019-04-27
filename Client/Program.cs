e60604fe6e1eda0077d0e7fcf4e95d52a6563a5c
Harikeshi <sshchegolikhin@gmail.com>
2019-04-27 22:56:41 +0300

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Serialization;
+
+public class UdpFileClient
+{
+    // Детали файла
+    [Serializable]
+    public class FileDetails
+    {
+        public string FILETYPE = "";
+        public long FILESIZE = 0;
+    }
+
+    private static FileDetails fileDet;
+
+    // Поля, связанные с UdpClient
+    private static int localPort = 5002;
+    private static UdpClient receivingUdpClient = new UdpClient(localPort);
+    private static IPEndPoint RemoteIpEndPoint = null;
+
+    private static FileStream fs;
+    private static Byte[] receiveBytes = new Byte[0];
+
+    [STAThread]
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Server started...");
+        // Получаем информацию о файле
+        GetFileDetails();
+
+        // Получаем файл
+        ReceiveFile();
+    }
+    private static void GetFileDetails()
+    {
+        try
+        {
+            // Получаем информацию о файле
+            receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+            XmlSerializer fileSerializer = new XmlSerializer(typeof(FileDetails));
+            MemoryStream stream1 = new MemoryStream();
+
+            // Считываем информацию о файле
+            stream1.Write(receiveBytes, 0, receiveBytes.Length);
+            stream1.Position = 0;
+
+            // Вызываем метод Deserialize
+            fileDet = (FileDetails)fileSerializer.Deserialize(stream1);
+            Console.WriteLine("File received " + fileDet.FILETYPE +
+                " size " + fileDet.FILESIZE.ToString() + " byte");
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+    }
+    public static void ReceiveFile()
+    {
+        try
+        {
+            // Получаем файл
+            receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+            // Создаем временный файл с полученным расширением
+            fs = new FileStream("temp." + fileDet.FILETYPE, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+            fs.Write(receiveBytes, 0, receiveBytes.Length);
+
+            Console.WriteLine("File Saved.");
+
+            // Открываем файл связанной с ним программой
+            Process.Start(fs.Name);
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+        finally
+        {
+            fs.Close();
+            receivingUdpClient.Close();
+            Console.Read();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Serialization;
+using System.Diagnostics;
+using System.Threading;
+
+public class UdpFileServer
+{
+    // Информация о файле (требуется для получателя)
+    [Serializable]
+    public class FileDetails
+    {
+        public string FILETYPE = "";
+        public long FILESIZE = 0;
+    }
+
+    private static FileDetails fileDet = new FileDetails();
+
+    // Поля, связанные с UdpClient
+    private static IPAddress remoteIPAddress;
+    private const int remotePort = 5002;
+    private static UdpClient sender = new UdpClient();
+    private static IPEndPoint endPoint;
+
+    // Filestream object
+    private static FileStream fs;
+    [STAThread]
+    static void Main(string[] args)
+    {
+        try
+        {
+            // Получаем удаленный IP-адрес и создаем IPEndPoint
+            remoteIPAddress = IPAddress.Parse("127.0.0.1");
+            //Console.WriteLine("Server Started ... ");
+            endPoint = new IPEndPoint(remoteIPAddress, remotePort);
+
+           
+            Console.Write("Enter File name:");
+            int leng = 0;
+
+
+           
+                fs = new FileStream(@Console.ReadLine().ToString(), FileMode.Open, FileAccess.Read);
+            
+            Console.Write("Enter max size:");
+            leng = Convert.ToInt32(Console.Read());
+
+            if (fs.Length > leng*10000)
+            {
+                Console.WriteLine("Biggest file!!");
+                sender.Close();
+                fs.Close();
+                return;
+            }
+           
+
+                // Отправляем информацию о файле
+                SendFileInfo();
+
+                // Ждем 2 секунды
+                Thread.Sleep(2000);
+
+                // Отправляем сам файл
+                SendFile();
+            
+            Console.ReadLine();
+
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+    }
+    public static void SendFileInfo()
+    {
+
+        // Получаем тип и расширение файла
+        fileDet.FILETYPE = fs.Name.Substring((int)fs.Name.Length - 3, 3);
+
+        // Получаем длину файла
+        fileDet.FILESIZE = fs.Length;
+
+        XmlSerializer fileSerializer = new XmlSerializer(typeof(FileDetails));
+        MemoryStream stream = new MemoryStream();
+
+        // Сериализуем объект
+        fileSerializer.Serialize(stream, fileDet);
+
+        // Считываем поток в байты
+        stream.Position = 0;
+        Byte[] bytes = new Byte[stream.Length];
+        stream.Read(bytes, 0, Convert.ToInt32(stream.Length));
+        
+
+        Console.WriteLine("Sending File...");
+
+        // Отправляем информацию о файле
+        sender.Send(bytes, bytes.Length, endPoint);
+        stream.Close();
+
+    }
+    private static void SendFile()
+    {
+        // Создаем файловый поток и переводим его в байты
+        Byte[] bytes = new Byte[fs.Length];
+        fs.Read(bytes, 0, bytes.Length);
+
+        try
+        {
+            // Отправляем файл
+            sender.Send(bytes, bytes.Length, endPoint);
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+        finally
+        {
+            // Закрываем соединение и очищаем поток
+            fs.Close();
+            sender.Close();
+        }
+        Console.WriteLine("File Sent.");
+        Console.Read();
+    }
+}
